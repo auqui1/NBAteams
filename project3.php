689b60fbc85fba6df406b8f1a328b5ebe7f74ab9
auqui1 <60719142+auqui1@users.noreply.github.com>
2020-02-06 00:48:36 -0500

    Create project3.php

---
+++ project3.php
@@ -0,0 +1,65 @@
+<?php
+
+$server="localhost";//server
+$user="root";//username
+$password="";//password
+$database="projectdb";
+//mysql_connect() is used to connect
+$mycon = mysqli_connect($server,$user,$password,$database) or die("no connection");
+
+?>
+
+<html>
+<head>
+<!-- title for web page -->
+<title>Top Five recent Team Detail</title>
+<!-- link to external style sheet -->
+<link href="project4.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+
+//select query
+$sqlQuery="select * from projectdb_table ORDER by Year DESC LIMIT 5";
+$res=mysqli_query($mycon,$sqlQuery);
+
+if(!$res)
+{
+die("Query Failed!!!");
+}
+?>
+
+<table class="table">
+<tr>
+<td>Id</td>
+<td>Team</td>
+<td>Year</td>
+</tr>
+<?php
+
+//mysql_fetch_array() will return result row by row
+while($row=mysqli_fetch_array($res))
+{
+?>
+<tr>
+<!-- display Team ID -->
+<td><?php echo $row{'Id'}?></td>
+<!-- display Team Name -->
+<td><?php echo $row{'Team'}?></td>
+<!-- display Year -->
+<td><?php echo $row{'Year'} ?></td>
+</tr>
+<?php }?>
+
+</table>
+<?php
+
+// close the database
+
+mysqli_close($mycon);
+?>
+<a href="project.html">Click to go back</a>
+
+</body>
+</html>
